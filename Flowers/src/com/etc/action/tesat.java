ee09cd83bd765987089a3682076a3246753ef61d
Administrator <Administrator@SKY-20160720ZYS>
2016-11-15 10:38:12 +0800

    22

--- Flowers/src/com/etc/action/tesat.java
+++ Flowers/src/com/etc/action/tesat.java
@@ -4,5 +4,18 @@ public class tesat {
 	public void test(){
 		System.out.println("2314qarws");
 	}
+	private String aa;
+	public void name() {
+		System.out.println();
+	}
+
+	public String getAa() {
+		return aa;
+	}
+
+	public void setAa(String aa) {
+		this.aa = aa;
+	}
+	
 
 }
