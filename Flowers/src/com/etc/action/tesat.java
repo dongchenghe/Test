e92bd2404df953ce474a9659b9ffc2603b51f35b
HDC <HDC@169.254.97.254>
2016-11-15 11:01:42 +0800

    fstdf

--- Flowers/src/com/etc/action/tesat.java
+++ Flowers/src/com/etc/action/tesat.java
@@ -2,24 +2,9 @@ package com.etc.action;
 
 public class tesat {
 
-	public void test(){
-		System.out.println("2314qarws");
-	}
 	private String aa;
 	public void name() {
 		System.out.println();
 	}
-
-
-
-
-	public String getAa() {
-		return aa;
-	}
-
-	public void setAa(String aa) {
-		this.aa = aa;
-	}
-	
 
 }
