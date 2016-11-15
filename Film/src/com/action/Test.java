96168c975f6abac159639f9699355eca6e946c57
Administrator <Administrator@SKY-20160720ZYS>
2016-11-15 15:31:58 +0800

    111

--- Film/src/com/action/Test.java
+++ Film/src/com/action/Test.java
@@ -3,10 +3,14 @@ package com.action;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("OK");
 
 	}
 	public void foo(){
 		System.out.println("dp");
 	}
+	public void say() {
+		System.out.println("呵呵");
+	}
+
 }
